786f657e6368084e45a3863611e5a525061f6db2
dogbutcat <dogbutcat@hotmail.com>
2016-01-31 15:01:16 +0800

    Update Qunit
    Need Environment

--- JavaScriptApplicationBook/funcDeclaration/funcA.js
+++ JavaScriptApplicationBook/funcDeclaration/funcA.js
@@ -2,22 +2,39 @@
  * Created by oliver on 1/28/16.
  */
 
-TestCase("function Region", {
-    "test grade": function () {
-        var score = 6;
+/*TestCase("function Region", {
+ "test grade": function () {
+ var score = 6;
 
-        if (score > 5 ) {
-            function grade(){
-                return 'pass!';
-            }
-        } else {
-            function grade(){
-                return 'fail!';
-            }
+ if (score > 5 ) {
+ function grade(){
+ return 'pass!';
+ }
+ } else {
+ function grade(){
+ return 'fail!';
+ }
+ }
+ assertEquals("this test is failed!","pass!",grade());
+ },
+ setUp: function () {
+ }
+ });*/
+
+TestCase("function Expressions. ", {
+    "test function scope": function () {
+        var a = function x() {
+            console.log('a');
+            assertEquals("x is usable inside the function!", "a", x);
+        };
+
+        a();
+
+        try {
+            x();
         }
-        assertEquals("this test is failed!","pass!",grade());
-    },
-    setUp: function () {
-
+        catch (e) {
+            console.log(e);
+        }
     }
-})
+});
